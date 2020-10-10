d057509419ff095e6e7521c562aea4bcc60071a9
Fabrice SAINTILMA <brucygio123@gmail.com>
2020-10-09 21:15:47 -0400

    adding two sub-classes

--- Port1.h
+++ Port1.h
@@ -21,4 +21,25 @@ protected:
     float prototype_field;
 };
 
+class ConcretePrototype1:public Prototype
+{
+public:
+    ConcretePrototype1(string prototype_name,float concrete_prototype_field)
+      :prototype(prototype_name),concrete_prototype_field1_(concrete_prototype_field){};
+
+      Prototype *Clone() const override;
+private:
+    float concrete_prototype_field1_;
+};
+
+class ConcretePrototype2:public Prototype
+{
+public:
+    ConcretePrototype2(string prototype_name,float concrete_prototype_field)
+      :prototype(prototype_name),concrete_prototype_field2_(concrete_prototype_field){};
+
+      Prototype *Clone() const override;
+private:
+    float concrete_prototype_field2_;
+};
 #endif // PORT1_H_INCLUDED
--- Prot1.cpp
+++ Prot1.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-Prototype:: virtual void Method(float prototype_field)
+Prototype::Prototype virtual void Method(float prototype_field)
 {
     this->prototype_field_=prototype_field;
     cout<<"Call method from "<<prototype_name_<<"with field: "<<prototype_field<<endl;
