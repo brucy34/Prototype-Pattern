6d83a19bb4c3aed4224cd4f7616f889dd52b3ed8
Fabrice SAINTILMA <brucygio123@gmail.com>
2020-10-09 21:22:26 -0400

    Cloning

--- Prot1.cpp
+++ Prot1.cpp
@@ -2,8 +2,18 @@
 
 using namespace std;
 
-Prototype::Prototype virtual void Method(float prototype_field)
+Prototype::virtual void Method(float prototype_field)
 {
     this->prototype_field_=prototype_field;
     cout<<"Call method from "<<prototype_name_<<"with field: "<<prototype_field<<endl;
 }
+
+ Prototype::Prototype *Clone() const override
+ {
+     return new ConcretePrototype1(*this);
+ }
+
+ Prototype::Prototype *Clone() const override
+ {
+     return new ConcretePrototype2(*this);
+ }
